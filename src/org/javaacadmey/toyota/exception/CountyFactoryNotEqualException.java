8b1612736b9d10394c88ed153c6adffded612a7a
Korelov Maksim <korelov@gmail.com>
2024-01-12 11:19:17 +0300

    delete Toyota

--- src/org/javaacadmey/toyota/exception/CountyFactoryNotEqualException.java
+++
@@ -1,7 +0,0 @@
-package org.javaacadmey.toyota.exception;
-
-public class CountyFactoryNotEqualException extends Exception {
-    public CountyFactoryNotEqualException(String message) {
-        super(message);
-    }
-}
