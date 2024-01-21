e51ed96225a9e0074f1f1f1f67fef13dc3b7bcc2
Korelov Maksim <korelov@gmail.com>
2024-01-21 16:11:07 +0300

    m2,hw2 ex1

--- src/org/homework/module2/two/ex1/Worker.java
+++ src/org/homework/module2/two/ex1/Worker.java
@@ -3,13 +3,16 @@ package org.homework.module2.two.ex1;
 import java.util.Queue;
 
 public class Worker {
-    private String name;
+    private final String name;
 
     public Worker(String name) {
         this.name = name;
     }
 
     public void throwSuitcase(Queue<Suitcase> suitcases) {
+        if (suitcases.isEmpty()) {
+            return;
+        }
         System.out.println(name + ": Мужики, кидаю чемодан " + suitcases.poll().getFlightNumber());
     }
 }
