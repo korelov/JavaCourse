b04a9b751359e86715a68f65a097ff13916711a4
Korelov Maksim <korelov@gmail.com>
2024-01-24 20:27:55 +0300

    m2,hw2 done

--- src/org/homework/module2/two/ex3/Runner.java
+++ src/org/homework/module2/two/ex3/Runner.java
@@ -1,13 +1,14 @@
 package org.homework.module2.two.ex3;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Runner {
     public static void main(String[] args) {
         Detail detail1 = new Detail("Пила");
         Detail detail2 = new Detail("Молоток");
         Detail detail3 = new Detail("Отвертка");
-        HashMap<Detail, Integer> map = new HashMap<>();
+        Map<Detail, Integer> map = new HashMap<>();
         map.put(detail1, 1);
         map.put(detail2, 2);
         map.put(detail3, 3);
